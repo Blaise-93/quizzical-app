0c768b722571ab4ee4deb644054f8141d1b9e5d8
Blaise <ejikemeblaise08@gmail.com>
2023-08-16 18:00:40 +0100

    responsive design for all devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,281 +1,284 @@
-import  {useState} from "react"
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import axios from "axios";
+import { decode } from "he";
+import Trivia from "./containers/Trivia";
+import { categories } from "./containers/data";
 
-import { nanoid } from 'nanoid'
-import { decode } from 'he'
-import axios from 'axios'
-
-import Trivia from './containers/Trivia'
-import {categories} from './containers/data'
-
-const App = () => {
-
-    const [quizzal, setQuizzical] = useState(null)
-    const [selected, setSelected] = useState([])
-    const [isChecked, setIsChecked] = useState(false)
-    const [message, setMessage] = useState('')
-    const [params, setParams] = useState({
-        difficulty: 'any',
-        category: 0,
-        type: 'any'
-    })
+function App() {
+  const [quizzical, setQuizzical] = useState(null);
+  const [selected, setSelected] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
+  const [message, setMessage] = useState("");
+  const [parameters, setParameters] = useState({
+    difficulty: "any",
+    category: 0,
+    type: "any",
+  });
 
 
-    /** A function that retrieves the user result lengths after
+  /** A function that retrieves the user result lengths after
      * it's been filtered out and returned the answer that matched
      * the the one provided by the opentdb.
      */
-    const userResult = JSON.stringify(selected) !== "{}" && selected
-    .filter((answer) => quizzal
-        .some((trivia) => trivia.id === answer.triviaId &&
-         trivia.correct_answer === answer.value
-         ) 
-    ).length
+  const result =
+    JSON.stringify(selected) !== "{}" && selected.filter((answer) =>
+      quizzical.some(
+        (trivia) =>
+          trivia.id === answer.triviaId &&
+          trivia.correct_answer === answer.value
+      )
+    ).length;
 
-
-    /** Renders html data gotten from quiz [opentdb] database.  */
-    const quizzHtmlData = quizzal ? quizzal.map((trivia, index) => (
+  const quizHtmlData = quizzical
+    ? quizzical.map((trivia) => (
         <Trivia
-            key={index}
-            trivia={trivia}
-            selected={
-                selected.filter(answer => answer.trivia === trivia)[0] || {}     
-            }
-            handleClick={handleClick}
-            isChecked={isChecked}
+          key={trivia.id}
+          trivia={trivia}
+          selected={
+            selected.filter((answer) => answer.triviaId === trivia.id)[0] || {}
+          }
+          handleClick={handleClick}
+          isChecked={isChecked}
         />
-    )): [] 
-    
+      ))
+    : [];
+
+  function shuffleArr(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    return arr;
+  }
+
+
+  /** Fetch the required Quizzical objects from the database as a promise and returns 
+   * the stateless updated proof of truth for the user playing the game. If the
+   * promise fails, then error is thrown at the user.
+   */
+  async function startQuiz(event) {
+    !isChecked && event.preventDefault();
+
+    let url = "https://opentdb.com/api.php?amount=5";
+
+    const params = Object.entries(parameters).filter(
+      ([key, value]) =>
+        ((key === "difficulty" || key === "type") && value !== "any") ||
+        (key === "category" && value !== 0)
+    );
+
+    if (params.length > 0) {
+      params.forEach(([key, value]) => {
+        url += `&${key}=${value}`;
+      });
+    }
+
+   try {
+    const res = await axios.get(url);
+    const data = await res.data;
+    console.log(data.results)
+    setQuizzical(
+      data.results.map((el) => ({
+
+        ...el,
+        correct_answer: decode(el.correct_answer),
+        incorrect_answers: el.incorrect_answers.map((answer) => 
+        decode(answer)),
+        question: decode(el.question),
+        id: nanoid(),
+        answers: shuffleArr([...el.incorrect_answers, el.correct_answer])
+        .map(
+          (answer) => ({
+            value: decode(answer),
+            id: nanoid(),
+          })
+        ),
+      }))
+    );
+   } catch (error) {
+        `Something went wrong! Kindly refresh your browser.`
+   }
+  }
+
 
     /** Provides a click event  which helps to select the data
-     * needed by the user via making sure that the triviaIndex
-     * matches with the user given input.
-      */
-    const handleClick = (traviaId, resultId, value) => {
-        setSelected(prevState => {
-            let newSelected
-            const triviaIndex  = prevState
-                .findIndex(answer => answer.triviaId === traviaId)
-            
-            if(triviaIndex >= 0) {
-                newSelected = [...prevState]
-                newSelected[triviaIndex] = {
-                    traviaId,
-                    resultId,
-                    value
-                }
-            }
-            else {
-                newSelected = [
-                    ...prevState, {traviaId, resultId, value}
-                ]
-            }
-       
-        })
-    }
+   * needed by the user via making sure that the triviaIndex
+   * matches with the user given input.
+    */
 
-    /** Fetch the required Quizzical objects from the database as a promise and returns 
-     * the stateless updated proof of truth for the user playing the game. If the
-     * promise fails, then error is thrown at the user.
-     */
-    const startQuizzal = async event => {
-        !isChecked && event.preventDefault();
-        let url =  "https://opentdb.com/api.php?amount=5";
-
-        const dataType = Object.entries(params).filter(
-         ([key, value], index) => 
-         ((key === 'difficulty' || key === 'type') && value !== 'any')
-         || (key === 'category' && value != 0)     
-        )
-
-        if(dataType.length > 0) {
-            dataType.forEach(([key, value]) => {
-                url += `&${key}=${value}`
-            })
-        }
-
-        try {
-            const res = await axios.get(url)
-            const data = res.data
-            console.log(data)
-
-            setQuizzical(
-                data.results.map((arr) => ({
-                    ...arr,
-                    correct_answer: decode(arr.correct_answer),
-                    incorrect_answers: arr.incorrect_answers.map((answer) => {
-                        return decode(answer)
-                    }),
-                    question: decode(el.question),
-                    id: nanoid(),
-                    answers: shuffleArr([...arr.incorrect_answers, el.correct_answer])
-                        .map(answer => ({
-                            value: decode(answer),
-                            id: nanoid()
-                        })) 
-                    })
-                
-                 )
-
-                )
-        } catch (error) {
-           <p className="error-message">
-                Something went wrong! Kindly refresh your browser.
-           </p>
-        }
-    }
-
-    /** Randomnly shuffle the incomming data of answers to reduce the
-     * user's prediction. 
-      */
-    const shuffleArr = arr => {
-        for (let i = arr.length - 1;  i > 0; --i) {
-            const randomNum =  Math.floor(Math.random() * (1 + i))
-            const loopNum = arr[i]
-            arr[i] = arr[randomNum]
-            arr[randomNum] = loopNum
-        }
-        console.log(arr)
-        return arr
-    }
-
-
-    /** Strickly check user results and send a useful message about the user/client 
+  function handleClick(triviaId, answerId, value) {
+    setSelected((prevSelected) => {
+      let newSelected;
+      const triviaIndex = prevSelected.findIndex(
+        (answer) => answer.triviaId === triviaId
+      );
+      if (triviaIndex >= 0) {
+        newSelected = [...prevSelected];
+        newSelected[triviaIndex] = {
+          triviaId,
+          answerId,
+          value,
+        };
+      } else {
+        newSelected = [
+          ...prevSelected,
+          {
+            triviaId,
+            answerId,
+            value,
+          },
+        ];
+      }
+      return newSelected;
+    });
+  }
+ 
+ 
+  /** Strickly check user results and send a useful message about the user/client 
      * state of the quiz.
-       */
-    const checkUserResult = () => {
-        if(selected.length === quizzal.length) {
-            setIsChecked(true)
-            if (userResult >= 4) {
-                setMessage(`Excellent job! Your scored ${userResult} / ${quizzal.length} in your quiz.`)
-            }
-            else {
-                setMessage(`Your scored ${userResult} / ${quizzal.length} in your quiz.\
-                Work hard next time.`)
-            }
-        }else {
-            setMessage("Please answer all the questions given prior to checking the results")
+  */
+  const checkUserResult = () => {
+    if (selected.length === quizzical.length) {
+      setIsChecked(true);
+        if(result === 5) {
+          setMessage(`Excellent👏! You scored ${result} / ${quizzical.length}
+           correct answers`);
+        }
+
+        else if (result >= 3) {
+          setMessage(`Good score 👍! You scored ${result} / ${quizzical.length}
+           correct answers`);
+        }
+        else {
+          setMessage(` You scored ${result} / ${quizzical.length} 
+          correct answers. Very Poor result, don't be discouraged.
+           Kindly try again😍 `);
         }
     }
-
-
-    /** A function to keep the user abreast of the game. */
-    const continuePlaying = async () => {
-        await startQuizzal()
-        setIsChecked(false)
-        setSelected([])
-        setMessage("")
+     else {
+      setMessage("Please answer all questions before checking the results");
     }
+  }
 
-    /** Set the options of the parameters of the quiz  */
-    const setOptions = e => {
-        const event = e.target.name
-        setParams(prevState => ({
-            ...prevState,
-            [event]: e.target.value
-        }))
-    }
-    
-    /** A function to keep the user in the loop of continuation of the game
+ /** A function to keep the user abreast of the game. */
+  const continuePlaying = async () => {
+     await startQuiz();
+    setIsChecked(false);
+    setSelected([]);
+    setMessage("");
+  }
+
+
+  /** Set the options of the parameters of the quiz  */ 
+  const setOptions = event => {
+    const name = event.target.name;
+    setParameters((prevParam) => ({
+      ...prevParam,
+      [name]: event.target.value,
+    }));
+  }
+
+
+/** A function to keep the user in the loop of continuation of the game
      * if the user wishes.
-       */
-    const restart = () => {
-        setIsChecked(false)
-        setSelected([])
-        setMessage('')
-        setQuizzical(null)
+  */
+  function restart() {
+    setIsChecked(false);
+    setSelected([]);
+    setMessage("");
+    setQuizzical(null);
+  }
 
-    }
-   
 
-    return (
-        <div className='quiz__container'>
-            {!quizzal ? (
-                
-                <div className="quizzal__container">
-                    <img src='src/assets/start1.png' className='img__one' />
-                    <img src='src/assets/start2.png' className='img__two' />
-                    
-                    <h1 className='start__title'>Quizzical</h1>
-                    <p>The best trivia you can ever imagine.</p>
-                    <form action="" className='params__form'>
-                        <div className='params__container'>
-                            <label htmlFor="difficulty">Choose difficulty level</label>
-                            <select               
-                                name="difficulty"
-                                id="difficulty"
-                                value={ params.difficulty }
-                                onChange={ setOptions }
-                            >
-                                <option value="any">Any difficulty</option>
-                                <option value="easy">Easy</option>
-                                <option value="medium">Medium</option>
-                                <option value="hard">Hard</option>
-                            </select>
-                        </div>
-                        <div className='params__container'>
-                            <label htmlFor='category'>Kindly choose category:</label>
-                            <select name="category" id="category"
-                                value={ params.category }
-                                onChange={ setOptions }>
-                                <option value={0}>Any categ</option>
-                                    {  categories.trivia_categories
-                                    .map((category) => (
-                                        <option key={category.id}
-                                        value={categories.id}>
-                                            {category.name}
-                                        </option>
-                                    ))}
-
-                                </select>
-                        </div>  
-                    <div className='params__container'>
-                        <label htmlFor="type">Choose type:</label>
-                        <select 
-                            name="type" id="type"
-                            value={params.type}
-                            onChange={setOptions}
-                            >
-                            <option value="any">Any type</option>
-                            <option value="multiple">Multiple choice</option>
-                            <option value="boolean">True/False</option>
-                          
-                        </select>
-                    </div>  
-                    <button
-                        className='start__btn' onClick={startQuizzal}
-                        >Start quiz 
-                    </button>
-                    </form>
-                </div>
-            ): (
-                <div>
-                    <img src="src/assets/quiz1.png" className="img__one" />
-                    <img src="src/assets/quiz2.png" className="img__two" />
-                    <div className="trivias__container">
-                        {quizzHtmlData}
-                        {!isChecked ? ( 
-                            <div className="trivias__results">
-                            <button className="trivias__btn" onClick={ checkUserResult }>
-                              Check answers
-                            </button>
-                            <span className="results__warning">{ message }</span>
-                          </div>
-                        ) : (
-                            <section className="trivias__results">
-                                <span className="results__display">{ message }</span>
-                                <button className="trivias__btn" onClick={ continuePlaying }>
-                                    Play Again 
-                                </button>
-                                <button className="restart__btn" onClick={restart}>
-                                    Resest parameters
-                                </button>
-                            </section>       
-                        )}
-                    </div>
-                </div>
-            )}
+  return (
+    <div className="quiz__container">
+      {!quizzical ? (
+        <div className="start__container">
+           <img src='src/assets/start1.png'  alt='quiz background logo' className='img__one' />
+            <img src='src/assets/start2.png' alt='quiz background logo' className='img__two' />
+            <h1 className="start__title">Quizzical</h1>
+          <p>The best trivia in town</p>
+          <form className="parameters__form">
+            <div className="params__container">
+              <label htmlFor="difficulty" className="difficulty">Choose difficulty level:</label>
+              <select
+                name="difficulty"
+                id="difficulty"
+                value={parameters.difficulty}
+                onChange={setOptions}
+              >
+                <option value="any">Any difficulty</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+            </div>
+            <div className="params__container">
+              <label htmlFor="category" className="difficulty">Choose category:</label>
+              <select
+                name="category"
+                id="category"
+                value={parameters.category}
+                onChange={setOptions}
+              >
+                <option value={0}>Any category</option>
+                {categories.trivia_categories.map((cat) => (
+                  <option key={cat.id} value={cat.id}>
+                    {cat.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="params__container">
+              <label htmlFor="type" className="difficulty">Choose type:</label>
+              <select
+                name="type"
+                id="type"
+                value={parameters.type}
+                onChange={setOptions}
+              >
+                <option value="any">Any type</option>
+                <option value="multiple">Multiple choice</option>
+                <option value="boolean">True / False</option>
+              </select>
+            </div>
+            <button className="start__btn" onClick={startQuiz}>
+              Start quiz
+            </button>
+          </form>
         </div>
-    )
+      ) : (
+        <div>
+           <img src="src/assets/quiz1.png"  alt="quiz background logo" className="img__one" />
+            <img src="src/assets/quiz2.png" alt='quiz background logo' className="img__two" />
+            <div className="trivias__container">
+            {quizHtmlData}
+            {!isChecked ? (
+              <div className="trivias__results">
+                <button className="trivias__btn" onClick={checkUserResult}>
+                  Check answers
+                </button>
+                <span className="results__warning">{message}</span>
+              </div>
+            ) : (
+              <div className="trivias__results">
+                <span className="results__display">{message}</span>
+                <button className="trivias__btn" onClick={continuePlaying}>
+                  Play again
+                </button>
+                <button className="restart__btn" onClick={ restart }>
+                  Reset parameters
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 
--- src/containers/Trivia.jsx
+++ src/containers/Trivia.jsx
@@ -1,46 +1,42 @@
-import React  from "react"
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+
+
 
 /** A function to pass the the user selected Trivia options on the game using
  * reacts props drilling principles.
   */
-function Trivia(props) {
-    console.log(props)
-    return (
-    <React.Fragment className='trivia__container'>
-        <h2 className="trivia__title">{ props.question }</h2>
-        <div className="answers__container">
-            {/**return the incomming array of trivia answers  */}
-            { props.trivia.answers.map(result => {
-                const isHeld = JSON.stringify(props.selected) != '{}' && 
-                props.selected.answerId === result.id
-                const isCorrect = props.trivia.correct_answer === result.value;
-                const isNotTheAnswer = isHeld && !isCorrect
-           
-                return (
-                    /** Do some routine checks on the user selected options.  */
-                    <span className={`${!props.isChecked && isHeld ? 'answer__held' : ''}
-                        ${ props.isChecked && isNotTheAnswer ? 'answer__red': ''} 
-                        ${
-                          props.isChecked && !isCorrect ? 'answer__gray': ''  
-                        }
-                        ${props.isChecked ? 'answer__disabled' : ''}
-                        
-                    `} key={result.id}
-                    onClick={() => props.handleClick(
-                                props.trivia, result.id, answer.value
-                                )}
-                    >
-                        {result.value}
-                    </span>
-                )
-            }) 
-            
-            }
-        </div>
-    </React.Fragment>
-    )
+function Trivia({ trivia, handleClick, selected, isChecked }){
+     {/**return the incomming array of trivia answers  */}
+  return (
+    <div className="trivia__container">
+      <h2 className="trivia__title">{trivia.question}</h2>
+      <div className="answers__container">
+        {trivia.answers.map((answer) => {
+          const isHeld =
+            JSON.stringify(selected) !== "{}" &&
+            selected.answerId === answer.id;
+          const isCorrect = trivia.correct_answer === answer.value;
+          const isIncorrect = isHeld && !isCorrect;
+          return (
+            /** Do some routine checks on the user selected options.  */
+            <span
+              className={`${!isChecked && isHeld ? "answer__held" : ""} ${
+                isChecked && isCorrect ? "answer__green" : ""
+              } ${isChecked && isIncorrect ? "answer__red" : ""} ${
+                isChecked && !isCorrect ? "answer__gray" : ""
+              } ${isChecked ? "answer__disabled" : ""}`}
+              key={answer.id}
+              onClick={() => handleClick(trivia.id, answer.id, answer.value)}
+            >
+              {answer.value}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
 
 /** Validators check on the user inputs/object selected.  */
     Trivia.ProtoTypes = {
